eb6962e9b59446ae4f35f41c60e43e755d977aa6
thusspokedata <a.sastre@vehiculum.de>
2024-07-04 19:22:49 +0200

    refactor: clean homepage, create fonts file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import '@mantine/core/styles.css';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import './globals.css';
-
-// const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "K'ooben",
