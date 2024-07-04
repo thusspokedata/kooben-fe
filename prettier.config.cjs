6aa01ae964e17b7fd05b5c4960610974c0831783
thusspokedata <thusspokedata@protonmail.com>
2024-07-04 18:27:56 +0200

    feat: gitignore, prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    arrowParens: 'always',
+    printWidth: 120,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    semi: true,
+    trailingComma: 'all',
+    tabWidth: 2,
+  };
