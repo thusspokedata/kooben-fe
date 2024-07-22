728e3897c18841730d08f183a1112a3340d0dc4c
thusspokedata <a.sastre@vehiculum.de>
2024-07-22 15:54:42 +0200

    refactor: quienes somos page

--- src/app/(shop)/quienes-somos/page.tsx
+++ src/app/(shop)/quienes-somos/page.tsx
@@ -2,29 +2,29 @@ import { QuienesSomos } from './QuienesSomos';
 import { Flex, Image } from '@mantine/core';
 import NextImage from 'next/image';
 
-const Historia = () => {
+interface HistoriaImageProps {
+  src: string;
+}
+
+const HistoriaImage = ({ src }: HistoriaImageProps) => (
+  <Image
+    component={NextImage}
+    radius="md"
+    height={500}
+    width={400}
+    fit="contain"
+    alt="Haciendo una instalación"
+    src={src}
+  />
+);
+
+const Historia: React.FC = () => {
   return (
     <>
       <QuienesSomos />
       <Flex justify="center" gap="11xl" mt="11xl" w="100%" align="center">
-        <Image
-          component={NextImage}
-          radius="md"
-          height={500}
-          width={400}
-          fit="contain"
-          alt="Haciendo una instalación"
-          src="/images/kooben-historia1.jpeg"
-        />
-        <Image
-          component={NextImage}
-          radius="md"
-          height={500}
-          width={400}
-          fit="contain"
-          alt="Haciendo una instalación"
-          src="/images/kooben-historia2.jpeg"
-        />
+        <HistoriaImage src="/images/kooben-historia1.jpeg" />
+        <HistoriaImage src="/images/kooben-historia2.jpeg" />
       </Flex>
     </>
   );
