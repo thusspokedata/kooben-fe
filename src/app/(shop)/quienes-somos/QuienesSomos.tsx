0cbbc237231cf6d44c30a914437fc1bf1a4294f9
thusspokedata <a.sastre@vehiculum.de>
2024-07-30 18:23:25 +0200

    refactor: historia and HeaderMegaMenu component

--- src/app/(shop)/quienes-somos/QuienesSomos.tsx
+++ src/app/(shop)/quienes-somos/QuienesSomos.tsx
@@ -1,67 +1,92 @@
 'use client';
 
-import { Title, Text, Box, Container, Stack, Card, useMantineTheme } from '@mantine/core';
+import { Title, Text, Box, Container, Stack, Card, TitleOrder } from '@mantine/core';
 
-export const QuienesSomos = () => {
-  const theme = useMantineTheme();
+interface SectionProps {
+  title: string;
+  order: TitleOrder | undefined;
+  children: React.ReactNode;
+}
+
+const Section: React.FC<SectionProps> = ({ title, order, children }) => (
+  <Box>
+    <Title order={order} fz="xl" fw={400} tt="uppercase" c="brand.7">
+      {title}
+    </Title>
+    {children}
+  </Box>
+);
+
+interface ValueTextProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const ValueText: React.FC<ValueTextProps> = ({ title, children }) => (
+  <Text fz="lg" c="brand.7">
+    <Box component="span" fw={600}>
+      {title}:&nbsp;
+    </Box>
+    {children}
+  </Text>
+);
+
+export const QuienesSomos: React.FC = () => {
   return (
     <Container size="lg">
       <Card padding="45px">
         <Stack c="brand.8">
-          <Box>
-            <Title order={1} size={theme.fontSizes['3xl']} fw={400} tt="uppercase">
-              Nuestra Historia
-            </Title>
-            <Text fz="lg">
+          <Section title="Nuestra Historia" order={1}>
+            <Text fz="lg" c="brand.7" fw={400}>
               En K&apos;OOBEN, cada pieza de mobiliario que creamos cuenta una historia de tradición y dedicación. Somos
               una empresa familiar, nacida del amor por el diseño y el deseo de mejorar los espacios donde vivimos.
               Desde nuestros inicios, hemos trabajado incansablemente para ofrecer muebles que no solo embellecen, sino
               que también funcionalizan tu hogar, aportando un equilibrio perfecto entre estética y practicidad.
             </Text>
-          </Box>
+          </Section>
 
-          <Box>
-            <Title order={3} size={theme.fontSizes['3xl']} fw={400} tt="uppercase">
-              Misión
-            </Title>
-            <Text fz="lg">
+          <Section title="Misión" order={3}>
+            <Text fz="lg" c="brand.7">
               En K&apos;OOBEN, creemos que cada mueble debe ser una pieza especial que complemente y mejore la vida
               cotidiana de nuestros clientes. Nuestra misión es brindar muebles de alta calidad que combinan diseño y
               durabilidad, adaptándose a las necesidades y gustos de quienes confían en nosotros. No se trata solo de
               llenar un espacio, sino de crear ambientes que inspiren y den vida a cada hogar.
             </Text>
-          </Box>
+          </Section>
 
-          <Box>
-            <Title order={3} size={theme.fontSizes['3xl']} fw={400} tt="uppercase">
-              Valores
-            </Title>
-            <Text fz="lg">
-              <Box component="span" fw={600}>
-                Calidad:&nbsp;
-              </Box>
+          <Section title="Valores" order={3}>
+            <ValueText title="Calidad">
               La calidad es el corazón de todo lo que hacemos. Utilizamos materiales de primera categoría para
               garantizar que cada mueble que sale de nuestro taller tenga la longevidad y belleza que nuestros clientes
               esperan y merecen.
-            </Text>
+            </ValueText>
 
-            <Text fz="lg">
-              <Box component="span" fw={600}>
-                Innovación:&nbsp;
-              </Box>
+            <ValueText title="Innovación">
               En un mundo en constante cambio, nos mantenemos a la vanguardia buscando nuevas tendencias y técnicas.
               Innovar es nuestra manera de asegurar que siempre te ofrecemos lo mejor.
-            </Text>
+            </ValueText>
 
-            <Text fz="lg">
-              <Box component="span" fw={600}>
-                Servicio al Cliente:&nbsp;
-              </Box>
+            <ValueText title="Servicio al Cliente">
               Sabemos que elegir un mueble es una decisión importante. Por eso, tu satisfacción es nuestra prioridad.
               Ofrecemos atención personalizada y asesoramiento experto para ayudarte a encontrar el mueble perfecto que
               se ajuste a tus necesidades y estilo.
+            </ValueText>
+          </Section>
+
+          <Section title="Descubre nuestra colección" order={3}>
+            <Text fz="lg" c="brand.7">
+              Explora nuestro catálogo y encuentra el mueble ideal para cada espacio de tu hogar. Desde un elegante
+              escritorio para tu oficina, un cómodo respaldo para tu cama, hasta un moderno mueble de TV, en
+              K&apos;OOBEN tenemos justo lo que necesitas para transformar tu casa en el hogar que siempre has soñado.
             </Text>
-          </Box>
+          </Section>
+
+          <Section title="Gracias por confiar!" order={3}>
+            <Text fz="lg" c="brand.7">
+              En K&apos;OOBEN, cada día trabajamos con la pasión y dedicación que caracteriza a una empresa familiar
+              comprometida con sus valores. Gracias por confiar en nosotros. Juntos, hacemos de tu casa un hogar.
+            </Text>
+          </Section>
         </Stack>
       </Card>
     </Container>
