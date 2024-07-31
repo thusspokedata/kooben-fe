4bf08a0a5aa2176e9c2e294fdf7aee7709b2f487
thusspokedata <thusspokedata@protonmail.com>
2024-08-01 00:27:21 +0200

    some images, working on homepage

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,22 @@
 'use client';
 
-import { Text } from '@mantine/core';
+import { CardsCarousel, Filters } from '@/components';
+import { Box, Container, Divider, Flex, Text, useMantineTheme } from '@mantine/core';
 
 export default function Home() {
+  const theme = useMantineTheme();
   return (
-    <>
-      <Text>Home Page</Text>
-    </>
+    <Container size="lg" p="lg" h="100%">
+      <Flex gap="xl" direction="column" h="100%">
+        <Filters />
+
+        <Text ta="center" size={theme.fontSizes['10xl']} c="#52796F" mt="xl" tt="uppercase">
+          Lo más vendido
+        </Text>
+
+        <CardsCarousel />
+        <Divider my="xl" size="lg" color="#52796F" />
+      </Flex>
+    </Container>
   );
 }
