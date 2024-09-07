4b094373ee8117b7c4ca1ae800b927f8fac7637f
thusspokedata <thusspokedata@protonmail.com>
2024-09-08 00:47:53 +0200

    refactor: clean code, add api call to getUser, some changes on carousel component

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,35 +1,22 @@
-'use client';
+import { BestSellersCarousel, Filters } from '@/components';
+import { Container, Flex, Text } from '@mantine/core';
+import getUsers from '../../api/get-user';
+import { auth, currentUser } from '@clerk/nextjs/server';
 
-import { CardsCarousel, Filters } from '@/components';
-import { ServiceOverview } from '@/components/home/ServiceOverview';
-import { Container, Divider, Box, Flex, Text, useMantineTheme } from '@mantine/core';
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
 
-export default function Home() {
-  const theme = useMantineTheme();
-
+export default async function Home() {
   return (
-    <>
-      <Container size="lg" px="lg" pt="lg" h="100%">
-        <Flex direction="column" h="100%">
-          <Filters />
-
-          <Box mt={{ base: 40, sm: 80 }}>
-            <Text
-              ta="center"
-              mb={theme.spacing['5xl']}
-              fz={{ base: theme.fontSizes['6xl'], sm: theme.fontSizes['10xl'] }}
-              c="secondary.6"
-              tt="uppercase"
-            >
-              Lo más vendido
-            </Text>
-            <CardsCarousel />
-
-            <Divider my={{ base: 40, sm: 82 }} size="lg" color="secondary.5" />
-          </Box>
-        </Flex>
-      </Container>
-      <ServiceOverview />
-    </>
+    <Container size="lg" px="lg" pt="lg" h="100%">
+      <Flex direction="column" h="100%">
+        <Filters />
+        <BestSellersCarousel />
+      </Flex>
+    </Container>
   );
 }
--- src/app/auth/new-account/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from '@mantine/core';
-
-export default function NewAccountPage() {
-  return (
-    <>
-      <Text c="blue.9">new account</Text>
-    </>
-  );
-}
