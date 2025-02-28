b9b31b7ad69fa2c2c640fb144d6ed920aa59646e
thusspokedata <thusspokedata@protonmail.com>
2025-02-28 20:19:14 +0100

    Add responsive padding to Container in Home page

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -4,7 +4,7 @@ import { Container, Flex } from '@mantine/core';
 export default async function Home() {
   return (
     <>
-      <Container size="lg" px="lg" pt="lg" h="100%">
+      <Container size="responsive" px={{base:"20px", xs:"40px", lg:"120"}} pt="lg" h="100%">
         <Flex direction="column" h="100%">
           <Filters />
           <BestSellersCarousel />
