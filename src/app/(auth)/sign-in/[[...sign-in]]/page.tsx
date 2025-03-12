a4419af70d800a603997596c6220e77d42104dcb
thusspokedata <thusspokedata@protonmail.com>
2025-03-12 14:15:09 +0100

    Refactor: Simplify auth pages by removing wrapper components

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,52 +1,7 @@
 'use client';
 
-import { ClerkLoaded, ClerkLoading, SignIn, useSignIn, useUser } from '@clerk/nextjs';
+import { ClerkLoaded, ClerkLoading, SignIn } from '@clerk/nextjs';
 import { Center, Loader } from '@mantine/core';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
-// This component verifies if the user exists in our database when logging in
-const SignInWrapper = () => {
-  const { isLoaded: isSignInLoaded, signIn, setActive } = useSignIn();
-  const { isLoaded: isUserLoaded } = useUser();
-  const router = useRouter();
-
-  // Effect to monitor changes in authentication state
-  useEffect(() => {
-    // We only monitor the sign-in state
-    if (!isSignInLoaded) return;
-
-    // Detect when the sign-in process is completed
-    if (signIn?.status === 'complete') {
-      // Activate session if necessary
-      if (signIn.createdSessionId) {
-        setActive({ session: signIn.createdSessionId })
-          .then(() => {
-            router.push('/');
-          })
-          .catch((err) => {
-            console.error('[SIGNIN FLOW] Error activating session:', err);
-          });
-      } else {
-        // If there's no createdSessionId but login is successful, redirect
-        router.push('/');
-      }
-    }
-  }, [isSignInLoaded, signIn?.status, signIn?.createdSessionId, setActive, router]);
-
-  // Show loading while Clerk components are loading
-  if (!isSignInLoaded || !isUserLoaded) {
-    return (
-      <Center mih="100vh" style={{ flexDirection: 'column' }}>
-        <ClerkLoading>
-          <Loader size={50} type="dots" />
-        </ClerkLoading>
-      </Center>
-    );
-  }
-
-  return <SignIn />;
-};
 
 export default function Page() {
   return (
@@ -56,7 +11,7 @@ export default function Page() {
       </ClerkLoading>
 
       <ClerkLoaded>
-        <SignInWrapper />
+        <SignIn />
       </ClerkLoaded>
     </Center>
   );
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,50 +1,7 @@
 'use client';
 
-import { SignUp, ClerkLoaded, ClerkLoading, useSignUp } from '@clerk/nextjs';
+import { SignUp, ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
 import { Center, Loader } from '@mantine/core';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
-// SignUp wrapper to handle redirects
-const SignUpWrapper = () => {
-  const { isLoaded, signUp, setActive } = useSignUp();
-  const router = useRouter();
-
-  // Effect to monitor sign-up status
-  useEffect(() => {
-    if (!isLoaded || !signUp) return;
-
-    // When sign-up is complete
-    if (signUp.status === 'complete') {
-      // Set session as active if available
-      if (signUp.createdSessionId) {
-        setActive({ session: signUp.createdSessionId })
-          .then(() => {
-            // Redirect to homepage
-            router.push('/');
-          })
-          .catch((err) => {
-            console.error('Error activating session:', err);
-            // Redirect anyway
-            router.push('/');
-          });
-      } else {
-        // If no session was created but sign-up is complete
-        router.push('/');
-      }
-    }
-  }, [isLoaded, signUp?.status, signUp?.createdSessionId, setActive, router, signUp]);
-
-  if (!isLoaded) {
-    return (
-      <Center>
-        <Loader size={50} type="dots" />
-      </Center>
-    );
-  }
-
-  return <SignUp />;
-};
 
 export default function Page() {
   return (
@@ -54,7 +11,7 @@ export default function Page() {
       </ClerkLoading>
 
       <ClerkLoaded>
-        <SignUpWrapper />
+        <SignUp />
       </ClerkLoaded>
     </Center>
   );
