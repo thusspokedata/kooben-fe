fb4d731de01fcc642d94345cabdab6ed45f3ff35
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2025-03-13 13:10:24 +0100

    feat: Implement product sizes and stock management features (#23)

--- src/services/getProductBySlug.ts
+++ src/services/getProductBySlug.ts
@@ -1,7 +1,16 @@
 import { api } from '@/api';
+import { Product } from '@/interfaces';
 
-export const getProductBySlug = async (slug: string) => {
-  const response = await api.get(`/products/${slug}`);
-  console.log('getProductBySlug', response.data);
-  return response.data;
+export const getProductBySlug = async (slug: string): Promise<Product> => {
+  try {
+    const response = await api.get(`/products/${slug}`);
+
+    // Extract the data from the response
+    const data = response.data as Product;
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching product by slug:', error);
+    throw error;
+  }
 };
